8b32d70553a315dacac5d58b87b958dcc4327a26
Cengizhan Dursun <cengizhandursun97@gmail.com>
2024-02-01 02:07:44 +0300

    PlayerMovement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,40 @@ public class Player : MonoBehaviour
 
     Vector2 rawInput;
     [SerializeField] float moveSpeed = 5f;
+
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingUp;
+    [SerializeField] float paddingDown;
+
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+    }
     private void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x-paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingDown, maxBounds.y - paddingUp);
+
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
